45768c5f5ed956d2b96d235026297d8848b1db07
jazy333 <jazy333@gmail.com>
2021-07-19 10:17:46 +0800

    minor fixed

--- src/core/index/index_reader.h
+++ src/core/index/index_reader.h
@@ -1,13 +1,14 @@
 #pragma once
-#include "sub_index_reader.h"
-
 #include <vector>
+
+#include "sub_index_reader.h"
 
 namespace yas {
 class IndexReader {
-    public:
-    std::vector<SubIndexReader*> get_sub_index_reader();
-    private:
-    std::vector<SubIndexReader*> sub_index_readers_;
+ public:
+  std::vector<SubIndexReader*> get_sub_index_reader();
+
+ private:
+  std::vector<SubIndexReader*> sub_index_readers_;
 };
 }  // namespace yas
